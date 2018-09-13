09440e9a787dcdff64a1238e05ed7546339cadc8
Erik Rigtorp <erik@rigtorp.se>
2018-09-13 02:49:47 -0500

    Use modern CMake layout

--- include/rigtorp/MPMCQueue.h
+++ include/rigtorp/MPMCQueue.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017 Erik Rigtorp <erik@rigtorp.se>
+Copyright (c) 2018 Erik Rigtorp <erik@rigtorp.se>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
